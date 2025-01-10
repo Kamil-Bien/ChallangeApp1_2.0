ec3fe86983f059b9971e1e7c04682bd4024d2451
Kamil-Bien <1kamilbien1@gmail.com>
2025-01-10 18:25:21 +0100

    Praca domowa nr 4

--- ChallangeApp1_2.0/ChallangeApp1_2.0/Program.cs
+++ ChallangeApp1_2.0/ChallangeApp1_2.0/Program.cs
@@ -1,2 +1,22 @@
-﻿Console.WriteLine("Hello, GoToIT");
-Console.WriteLine("homework 'day' 3");
+﻿//Kurs podstawowy C#
+//Praca domowa nr 4
+string name = "Rysio";
+char sex = 'M'; //M = Male
+                //F = Female
+int age = 30;
+if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == 'M' &&  age < 18)
+{
+    Console.WriteLine("Niepełnoletni Mężczyzna");
+}
+else
+{
+    Console.WriteLine("To tylko Rysio (potraktuj ten komunikat jako \"nic\")");
+}
