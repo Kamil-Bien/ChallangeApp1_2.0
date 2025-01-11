1d686e4abbe9c73999daaa089dd4aba9240f6459
Kamil-Bien <1kamilbien1@gmail.com>
2025-01-11 22:24:16 +0100

    Praca domowa nr 5

--- ChallangeApp1_2.0/ChallangeApp1_2.0/Program.cs
+++ ChallangeApp1_2.0/ChallangeApp1_2.0/Program.cs
@@ -1,22 +1,30 @@
 ﻿//Kurs podstawowy C#
-//Praca domowa nr 4
-string name = "Rysio";
-char sex = 'M'; //M = Male
-                //F = Female
-int age = 30;
-if (sex == 'F' && age < 30)
+//Praca domowa nr 5
+
+//deklaracja zmiennych oraz konwersja liczby podanej w typie int na tablice znaków (char)
+int number = 4650660; 
+string numberInString = number.ToString(); 
+char[] letters = numberInString.ToArray(); 
+int[] countNumber = new int[10];
+
+
+foreach (var oneNumber in letters) //pętla dla każdego elementu utworzonej tablicy
 {
-    Console.WriteLine("Kobieta poniżej 30 lat");
+    //w pętli wykonano dwie konwersje zmiennych w string w celach porównanwczych
+    string oneNumberString = oneNumber.ToString();
+    for (int i = 0; i < 10; i++) //pętla służąca do inkrementacji wystąpień danej cyfry w tablicy
+    {
+        string iString = i.ToString();
+        if (iString == oneNumberString)
+        {
+            countNumber[i]++;
+        }
+    }
 }
-else if (name == "Ewa" && age == 30)
+
+//wyświetlenie wyników powyższego procesu
+Console.WriteLine("cyfra => liczba wystąpień");
+for (int i = 0; i < 10; i++)
 {
-    Console.WriteLine("Ewa, lat 30");
-}
-else if (sex == 'M' &&  age < 18)
-{
-    Console.WriteLine("Niepełnoletni Mężczyzna");
-}
-else
-{
-    Console.WriteLine("To tylko Rysio (potraktuj ten komunikat jako \"nic\")");
+    Console.WriteLine(i + " => " + countNumber[i]);
 }
